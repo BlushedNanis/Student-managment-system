d7b2829c68c4d2bf9f627e7785afd4c257c20ad9
BlushedNanis <luis_danieltg@outlook.com>
2024-05-01 12:14:04 -0400

    Moved database creation

--- main.py
+++ main.py
@@ -30,21 +30,14 @@ class MainWindow(QMainWindow):
         self.load_data()
         
     def load_data(self):
-        conn = db.connect("database.db")
-        cur = conn.cursor()
-        cur.execute("SELECT name FROM sqlite_master WHERE name=='students'")
-        if cur.fetchone() is None:
-            cur.execute("CREATE TABLE students(id INTEGER PRIMARY KEY,"\
-                                                "name, course, phone)")
-            
-        data = cur.execute("SELECT * FROM students")
+        conn = db.connect("database.db")    
+        data = conn.execute("SELECT * FROM students")
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(data):
             self.table.insertRow(row_number)
             for column_number, cell_data in enumerate(row_data):
                 self.table.setItem(row_number, column_number,
                                    QTableWidgetItem(str(cell_data)))
-        cur.close()
         conn.close()
 
     def insert(self):
@@ -90,7 +83,18 @@ class InsertDialog(QDialog):
         conn.close()
         main_window.load_data()
         
-    
+
+conn = db.connect("database.db")
+cur = conn.cursor()
+cur.execute("SELECT name FROM sqlite_master WHERE name=='students'")
+if cur.fetchone() is None:
+    cur.execute("CREATE TABLE students(id INTEGER PRIMARY KEY,"\
+                                        "name VARCHAR(256),"\
+                                        "course VARCHAR(256),"\
+                                        "phone VARCHAR(256))")
+cur.close()
+conn.close()
+
 app = QApplication(argv)
 main_window = MainWindow()
 main_window.show()
